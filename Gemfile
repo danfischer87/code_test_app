13c18869ebb58187f039ee02142ad2ae6e018513
Dan Fischer <dan.fischer@priorilegal.com>
2015-01-29 19:46:01 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.21'
+gem 'sqlite3'
+gem 'devise'
+
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+  gem 'selenium-webdriver'
+  gem 'capybara-webkit'
+  gem 'rspec-instafail'
+  gem 'capybara-email'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeTestApp::Application.config.secret_token = '582ee9f128994bd6f38e0cee47839095c42a57afe2a1e79687c9668d2eec5bf89932566904b333d90ce32dca3055d867f55498da8fc102b8a4f311cd208f8abf'
