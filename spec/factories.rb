270c81df1f0a151d7b7261a713a2d86f3bba6972
Dan Fischer <dan.fischer@priorilegal.com>
2015-03-19 12:24:02 -0400

    Added some stuff for rspec and factories to get started faster

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+require 'factory_girl'
+
+FactoryGirl.define do
+  # factory :some_model do
+  #   some_column "hello world"
+  # end
+end
---
+++ spec/models/some_test_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+# describe SomeModel do
+#   let(:some_model) {create :some_model}
+# end
